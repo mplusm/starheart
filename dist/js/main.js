8058f91a097289e80c7393199748fe0a3b6af45e
mplusm <73212622+mplusm@users.noreply.github.com>
2020-12-30 00:58:49 -0500

    initial commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,7 +4,7 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll("nav-item");
+const navItems = document.querySelectorAll(".nav-item");
 
 //set the initial state of the menu
 let showMenu = false;
@@ -17,7 +17,8 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
-    navItems.forEach((item) => item.classList.add("show"));
+    navItems.forEach(item => item.classList.add("show"));
+
 
     //set menu state
     showMenu = true;
@@ -26,7 +27,7 @@ function toggleMenu() {
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
-    navItems.forEach((item) => item.classList.remove("show"));
+    navItems.forEach(item => item.classList.remove("show"));
 
     //set menu state
     showMenu = false;
